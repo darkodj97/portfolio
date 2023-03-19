91022dd5b27341d08af8fb2f666dbc6bdcf1b907
unknown <djurdjevdarko1997@gmail.com>
2023-03-19 22:35:45 +0100

    inital push

---
+++ script.js
@@ -0,0 +1,23 @@
+const tablinks = document.getElementsByClassName('tab-links');
+const tabcontents = document.getElementsByClassName('tab-contents');
+const sidemenu = document.getElementById('sidemenu');
+
+function opentab(tabname) {
+    for(tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for(tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+
+function closedmenu() {
+    sidemenu.style.right = "-200px";
+}
